4b97303dfa4486924ab7a2985b2581a0fc7a94fe
宇野礼於 <leouno12@gmail.com>
2019-11-26 10:22:49 +0900

    :pensive: rollback crud funcs to the previous API

--- src/PersistentUtil/Crud.hs
+++ src/PersistentUtil/Crud.hs
@@ -10,38 +10,35 @@ module PersistentUtil.Crud
   , delete'
   ) where
 
-import           Conduit                   ( MonadUnliftIO )
-import           Data.Pool                 ( Pool )
+import           Conduit              ( MonadUnliftIO )
+import           Data.Pool            ( Pool )
 import           Database.Persist
 import           Database.Persist.Sql
-
-import           PersistentUtil.Conversion ( entity2Tuple )
 
 selectList' ::
      ( PersistQueryRead backend
      , IsPersistBackend backend
-     , ToBackendKey SqlBackend b
-     , MonadUnliftIO f
-     , PersistEntityBackend b ~ BaseBackend backend
+     , MonadUnliftIO m
+     , PersistEntity record
+     , PersistEntityBackend record ~ BaseBackend backend
      , BaseBackend backend ~ SqlBackend
      )
   => Pool backend
-  -> f [(Int, b)]
-selectList' pool = map entity2Tuple <$> runSqlPool (selectList [] []) pool
+  -> m [Entity record]
+selectList' = runSqlPool $ selectList [] []
 
 get' ::
      ( PersistStoreRead backend
      , IsPersistBackend backend
-     , ToBackendKey SqlBackend b1
-     , MonadUnliftIO f
-     , PersistEntity (Entity b1)
+     , MonadUnliftIO m
+     , PersistEntity record
+     , PersistEntityBackend record ~ BaseBackend backend
      , BaseBackend backend ~ SqlBackend
-     , PersistEntityBackend (Entity b1) ~ BaseBackend backend
      )
-  => Key (Entity b1)
+  => Key record
   -> Pool backend
-  -> f (Maybe (Int, b1))
-get' k pool = fmap entity2Tuple <$> runSqlPool (get k) pool
+  -> m (Maybe record)
+get' k = runSqlPool $ get k
 
 insert' ::
      ( PersistStoreWrite backend
